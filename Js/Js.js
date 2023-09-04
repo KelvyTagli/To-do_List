ac5071bd7937966d75aafd8573e91f7746b3ad3d
Kelvy Tagli <kelvytagliacolli@hotmail.com.br>
2023-09-04 02:51:11 -0300

    finish todo List

--- Js/Js.js
+++ Js/Js.js
@@ -1,29 +1,53 @@
-const todoform = document.querySelector('#todo-form');
+const todoForm = document.querySelector('#todo-form');
 const todoInput = document.querySelector('#todo-input');
-const todobtn = document.querySelector('#todo-btn');
-const editform = document.querySelector('#todo-edit');
+const todo_btn = document.querySelector('#todo-btn');
+const editForm = document.querySelector('#todo-edit');
 const editInput = document.querySelector('#edit-input');
-const cancelbtn = document.querySelector('#btn-cancel');
-const todolist = document.querySelector('#todo-list');
+const edit_btn = document.querySelector('#edit-button');
+const edit_cancel = document.querySelector('#btn-cancel');
+const todoList = document.querySelector('#todo-list');
+let oldInput;
 
-//eventos 
-todobtn.addEventListener('click', (e) => {
+//Eventos
+todo_btn.addEventListener('click', (e) => {
     e.preventDefault();
 
     const input = todoInput.value;
     if (input) {
-        console.log(input);
-    } else {
-        alert('Preencha o campo de Adicione sua Tarefa');
+        saveTodo(input);
+    } 
+    else {
+        alert('Preencha o Campo de Adicione sua tarefa ');
     }
 
     todoInput.value = "";
     todoInput.focus();
 });
 
+edit_btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const newinputEdit = editInput.value;
+
+    if (newinputEdit) {
+        updateInput(newinputEdit);
+    }
+
+    toggleEdit();
+});
+
+edit_cancel.addEventListener('click', (e) => {
+    e.preventDefault();
+    toggleEdit();
+});
+
 document.addEventListener('click', (e) => {
     const targetEvent = e.target;
     const parentEl = targetEvent.closest('div');
+    let input_title;
+
+    if (parentEl && parentEl.querySelector('h3')) {
+        input_title = parentEl.querySelector('h3').innerText;
+    }
 
     if (targetEvent.classList.contains('finish-todo')) {
         parentEl.classList.toggle('todo-done');
@@ -31,6 +55,8 @@ document.addEventListener('click', (e) => {
 
     if (targetEvent.classList.contains('edit-todo')) {
         toggleEdit();
+        editInput.value = input_title;
+        oldInput = input_title;
     }
 
     if (targetEvent.classList.contains('remove-todo')) {
@@ -38,42 +64,47 @@ document.addEventListener('click', (e) => {
     }
 });
 
-cancelbtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    toggleEdit();
-})
-
 //Função
-const savetodo = (text) => {
-
+const saveTodo = (text) => {
     const todo = document.createElement('div');
     todo.classList.add('todo');
 
-    const todotitle = document.createElement('h3');
-    todotitle.classList.add('todo-title');
-    todotitle.innerText = text;
-    todo.appendChild(todotitle);
+    const todoTitle = document.createElement('h3');
+    todoTitle.classList.add('todo-title');
+    todoTitle.innerText = text;
+    todo.appendChild(todoTitle);
 
-    const btnfinish = document.createElement('button');
-    btnfinish.classList.add('finish-todo');
-    btnfinish.innerHTML = '<i class="material-icons" style="font-size: small;">check</i>';
-    todo.appendChild(btnfinish);
+    const btnFinish = document.createElement('button');
+    btnFinish.classList.add('finish-todo');
+    btnFinish.innerHTML = '<i class="material-icons" style="font-size: small;">check</i>';
+    todo.appendChild(btnFinish);
 
-    const btnedit = document.createElement('button');
-    btnedit.classList.add('edit-todo');
-    btnedit.innerHTML = '<i class="material-icons" style="font-size: small;">create</i>';
-    todo.appendChild(btnedit);
+    const btnEdit = document.createElement('button');
+    btnEdit.classList.add('edit-todo');
+    btnEdit.innerHTML = '<i class="material-icons" style="font-size: small;">create</i>';
+    todo.appendChild(btnEdit);
 
     const btnremove = document.createElement('button');
     btnremove.classList.add('remove-todo');
     btnremove.innerHTML = '<i class="material-icons" style="font-size: small;">clear</i>';
     todo.appendChild(btnremove);
 
-    todolist.appendChild(todo);
+    todoList.appendChild(todo);
 }
 
 const toggleEdit = () => {
-    todoform.classList.toggle('hide');
-    editform.classList.toggle('hide');
-    todolist.classList.toggle('hide');
+    todoForm.classList.toggle('hide');
+    todoList.classList.toggle('hide');
+    editForm.classList.toggle('hide');
+}
+
+const updateInput = (text) => {
+    const todo = document.querySelectorAll('.todo');
+
+    todo.forEach((todo) => {
+        let titleTask = todo.querySelector('h3');
+        if (titleTask.innerText === oldInput) {
+            titleTask.innerText = text;
+        }
+    });
 }
